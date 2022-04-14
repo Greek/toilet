81ef2671d397be1493dbac1cffbdfa53d10c8fb5
Andreas <me@apap04.com>
2022-04-13 23:02:18 -0400

    Update help.lua

--- help.lua
+++ help.lua
@@ -1,7 +1,22 @@
+KEY:
+  <> - required
+  [] - optional
+
 Not all commands may be documented. -- See more info here: https://discord.gg/YqkpR4g5dX
-1.
-2.
-3.
-4.
-5.
-6.
+
+Moderation:
+  1. ,ban <member> [reason] -- Ban a person.
+  2. ,kick <member> [reason] -- Kick a person.
+  3. ,clear <amount> -- Clear a certain amount of messages
+  4. ,mute <member> [duration (seconds)] -- Mute a person.
+  5. ,unmute <member> -- Un-mute a person.
+
+Configuration:
+  1. ,config logs messages set <channel> -- Set a message logging channel
+  2. ,config logs messages clear -- Clear the message log channel
+  3. ,config welcome set <channel> <message> -- Set a welcome channel and message. Special args: {user.name} {user.tag} {user.mention}
+
+Fun:
+  1. ,who [member] -- Get information about a person
+  2. ,xkcd [id] -- Get a xkcd comic
+  3. ,avatar [membe] -- Get a person's avatar.
