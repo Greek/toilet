c13fc7053e797d26cbafc6752cf005f3cc760d1e
Andreas <apap@null.net>
2022-04-24 22:26:42 -0400

    added new commands

--- help.lua
+++ help.lua
@@ -15,6 +15,10 @@ Configuration:
   1. ,config logs messages set <channel> -- Set a message logging channel
   2. ,config logs messages clear -- Clear the message log channel
   3. ,config welcome set <channel> <message> -- Set a welcome channel and message. Special args: {user.name} {user.tag} {user.mention}
+  4. ,filter add delete <phrase> -- Add a phrase to automagically delete (!!! no sentences !!!)
+  5. ,filter add ban <phrase> -- Add a phrase to automagically ban a user for (!!! no sentences !!!)
+  6. ,filter remove <phrase> -- Remove a phrase from the filter.
+  7. ,filter reset -- Reset the whole filter.
 
 Fun:
   1. ,who [member] -- Get information about a person
@@ -22,3 +26,4 @@ Fun:
   3. ,avatar [member] -- Get a person's avatar.
   4. ,snipe -- Get the last deleted message in the current channel.
   5. ,editsnipe -- Get the last edited message in the current channel.
+  6. ,userinfo [user] -- Get a person's information
