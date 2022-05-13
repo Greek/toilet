ca1f22f6c63a9d5e9e58b58b20b8d447c555eb36
Andreas <a@yeat.dev>
2022-05-13 10:57:28 -0400

    Update help.lua

--- help.lua
+++ help.lua
@@ -7,7 +7,7 @@ Not all commands may be documented. -- See more info here: https://discord.gg/Yq
 Moderation:
   1. ,ban <member> [reason] -- Ban a person.
   2. ,kick <member> [reason] -- Kick a person.
-  3. ,clear <amount> -- Clear a certain amount of messages
+  3. ,clear <amount> -- Clear a certain amount of messages.
   4. ,mute <member> [duration (seconds)] -- Mute a person.
   5. ,unmute <member> -- Un-mute a person.
   6. ,nuke -- Delete and re-create the current channel.
@@ -16,10 +16,18 @@ Configuration:
   1. ,config logs messages set <channel> -- Set a message logging channel
   2. ,config logs messages clear -- Clear the message log channel
   3. ,config welcome set <channel> <message> -- Set a welcome channel and message. Special args: {user.name} {user.tag} {user.mention}
-  4. ,filter add delete <phrase> -- Add a phrase to automagically delete (!!! no sentences !!!)
-  5. ,filter add ban <phrase> -- Add a phrase to automagically ban a user for (!!! no sentences !!!)
-  6. ,filter remove <phrase> -- Remove a phrase from the filter.
-  7. ,filter reset -- Reset the whole filter.
+
+Filter:
+  1. ,filter add delete <phrase> -- Add a phrase to automagically delete (!!! no sentences !!!)
+  2. ,filter add ban <phrase> -- Add a phrase to automagically ban a user for (!!! no sentences !!!)
+  3. ,filter remove <phrase> -- Remove a phrase from the filter.
+  4. ,filter links -- Toggle to filter links.
+  5. ,filter reset -- Reset the whole filter.
+
+Giveaways:
+  1. ,giveaway create -- Start the giveaway creation flow
+  2. ,config giveaway set <channel> -- Set a channel to send the giveaways to.
+  3. ,config giveaway clear -- Clear the current giveaway channel.
 
 Fun:
   1. ,who [member] -- Get information about a person
