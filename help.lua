cf8ae02d0ac6b988b0ed91a8498aab8248484535
Andreas <a@yeat.dev>
2022-05-24 03:10:01 -0400

    Update help.lua

--- help.lua
+++ help.lua
@@ -16,9 +16,12 @@ Roles:
   1. ,role <member> <role> -- Gives a role to a person who doesn't have it, removes it if they do.
 
 Configuration:
-  1. ,config logs messages set <channel> -- Set a message logging channel
-  2. ,config logs messages clear -- Clear the message log channel
-  3. ,config welcome set <channel> <message> -- Set a welcome channel and message. Special args: {user.name} {user.tag} {user.mention}
+  0. ,config logs messages set <channel> -- Set a message logging channel
+  0. ,config logs messages clear -- Clear the message log channel
+  0. ,config logs messages whitelist <channel> -- Add a channel to ignore messages from.
+  0. ,config logs messages unwhitelist <channel> -- Remove a channel from the logging whitelist.
+  0. ,config logs messages clearwhitelist -- Clear the logging whitelist.
+  0. ,config welcome set <channel> <message> -- Set a welcome channel and message. Special args: {user.name} {user.tag} {user.mention}
 
 Filter:
   1. ,filter add delete <phrase> -- Add a phrase to automagically delete (!!! no sentences !!!)
@@ -32,11 +35,12 @@ Giveaways:
   2. ,config giveaway set <channel> -- Set a channel to send the giveaways to.
   3. ,config giveaway clear -- Clear the current giveaway channel.
 
+Utilities:
+  0. ,userinfo [member] -- Get information about a person
+  0. ,avatar [member] -- Get a person's avatar.
+  0. ,emoji <emoji> -- Get the URL of an emoji.
+  0. ,snipe -- Get the last deleted message in the current channel.
+  0. ,editsnipe -- Get the last edited message in the current channel.
+  
 Fun:
-  1. ,who [member] -- Get information about a person
-  2. ,xkcd [id] -- Get a xkcd comic
-  3. ,avatar [member] -- Get a person's avatar.
-  4. ,snipe -- Get the last deleted message in the current channel.
-  5. ,editsnipe -- Get the last edited message in the current channel.
-  6. ,userinfo [user] -- Get a person's information
-  7. ,emoji <emoji> -- Get the URL of an emoji.
+  0. ,xkcd [id] -- Get a xkcd comic
